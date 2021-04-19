7d481c46a53a606e2d39dc7ae593eb65fb5409fa
Alexandr <tank006@yandex.ru>
2021-04-19 19:37:01 +0400

    Finish homework

--- src/main/java/HomeworkOne/homework/ArrayMas.java
+++ src/main/java/HomeworkOne/homework/ArrayMas.java
@@ -1,40 +1,40 @@
-package HomeworkOne.homework;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-public class ArrayMas {
-    //метод, где мы меняем два элемента массива ссылочного типа местами
-    // 1. Написать метод, который меняет два элемента массива местами.(массив может быть любого ссылочного типа);
-    public static void main(String[] args) {
-        Car car [] = new Car[5];
-        car [0] = new Car(4, "white", 200);
-        car [1] = new Car(4, "black", 100);
-        car [2] = new Car(2, "orange", 250);
-        car [3] = new Car(4, "red", 400);
-        car [4] = new Car(6, "white", 10);
-
-        for (int i = 0; i < car.length; i++) {
-            System.out.println(car[i].wheel + " " + car[i].color + " " + car[i].speed);
-        }
-
-        Car n = car[2];
-        car[2] = car[4];
-        car[4] = n;
-        System.out.println("а теперь мы поменяли местами");
-        for (int i = 0; i < car.length; i++) {
-            System.out.println(car[i].wheel + " " + car[i].color + " " + car[i].speed);
-        }
-
-
-        //преоброзование массива в лист ArrayList
-        //2. Написать метод, который преобразует массив в ArrayList;
-        List<Car> list1 = new ArrayList<>();
-
-        list1 = Arrays.asList(car);
-        for (Car c: list1) {
-            System.out.println(c.wheel + " " + c.color + " " + c.speed);
-        }
-    }
-}
+//package HomeworkOne.homework;
+//
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//import java.util.List;
+//
+//public class ArrayMas {
+//    //метод, где мы меняем два элемента массива ссылочного типа местами
+//    // 1. Написать метод, который меняет два элемента массива местами.(массив может быть любого ссылочного типа);
+//    public static void main(String[] args) {
+//        Car car [] = new Car[5];
+//        car [0] = new Car(4, "white", 200);
+//        car [1] = new Car(4, "black", 100);
+//        car [2] = new Car(2, "orange", 250);
+//        car [3] = new Car(4, "red", 400);
+//        car [4] = new Car(6, "white", 10);
+//
+//        for (int i = 0; i < car.length; i++) {
+//            System.out.println(car[i].wheel + " " + car[i].color + " " + car[i].speed);
+//        }
+//
+//        Car n = car[2];
+//        car[2] = car[4];
+//        car[4] = n;
+//        System.out.println("а теперь мы поменяли местами");
+//        for (int i = 0; i < car.length; i++) {
+//            System.out.println(car[i].wheel + " " + car[i].color + " " + car[i].speed);
+//        }
+//
+//
+//        //преоброзование массива в лист ArrayList
+//        //2. Написать метод, который преобразует массив в ArrayList;
+//        List<Car> list1 = new ArrayList<>();
+//
+//        list1 = Arrays.asList(car);
+//        for (Car c: list1) {
+//            System.out.println(c.wheel + " " + c.color + " " + c.speed);
+//        }
+//    }
+//}
--- src/main/java/HomeworkOne/homework/Car.java
+++ src/main/java/HomeworkOne/homework/Car.java
@@ -1,13 +1,13 @@
-package HomeworkOne.homework;
-
-public class Car {
-    int wheel;
-    String color;
-    int speed;
-
-    public Car(int wheel, String color, int speed) {
-        this.wheel = wheel;
-        this.color = color;
-        this.speed = speed;
-    }
-}
+//package HomeworkOne.homework;
+//
+//public class Car {
+//    int wheel;
+//    String color;
+//    int speed;
+//
+//    public Car(int wheel, String color, int speed) {
+//        this.wheel = wheel;
+//        this.color = color;
+//        this.speed = speed;
+//    }
+//}
